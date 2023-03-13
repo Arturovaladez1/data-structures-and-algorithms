5c5f9f5608d98e25fa50a452fe96c275e5e01709
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-03-12 22:45:39 -0700

    objects array code challenge

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -10,6 +10,9 @@ E.g. [4,2,7,5,9,2] -> 9
 ------------------------------------------------------------------------------------------------ */
 const maxInArray = (arr) => {
   // Solution code here...
+  return arr.reduce((prev, curr) => {
+    return Math.max(prev, curr);
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -19,13 +22,15 @@ Write a function named getCourseKeys that takes in the courseInfo object and ret
 
 For example: (['name', 'duration', 'topics', 'finalExam']).
 ------------------------------------------------------------------------------------------------ */
-const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningTrack: '8 weeks'},
+const courseInfo = {
+  name: 'Code 301', duration: { dayTrack: '4 weeks', eveningTrack: '8 weeks' },
   topics: ['SMACSS', 'APIs', 'NodeJS', 'SQL', 'jQuery', 'functional programming'],
   finalExam: true
 };
 
 const getCourseKeys = (obj) => {
   // Solution code here...
+  return Object.keys(obj);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -38,6 +43,7 @@ Write a function named checkValues that takes in an object and a value and retur
 
 const checkValues = (obj, value) => {
   // Solution code here...
+  return Object.values(obj).includes(value);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -61,6 +67,17 @@ HR has asked you to change the data to make it easier to print so that it looks 
 
 const updateNumbers = (obj) => {
   // Solution code here...
+  let values = Object.values(obj);
+  let keys = Object.keys(obj);
+
+  let combinedArr = [];
+  for (let key of keys) {
+    for (let value of values) {
+      combinedArr.push(`${key}: ${value}`)
+    }
+  }
+  return combinedArr;
+
 };
 
 
@@ -134,6 +151,9 @@ hasChildrenValues(characters, 'Sansa') will return false
 
 const hasChildrenValues = (arr, character) => {
   // Solution code here...
+  let houses = [];
+  houses = arr.map(obj => obj.house);
+  return houses;
 
 };
 
