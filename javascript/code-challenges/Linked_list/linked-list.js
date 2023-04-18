88271b6cf21b16f0dd2102decf1f61f80efa72af
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-04-17 23:56:20 -0700

    updated readme

---
+++ javascript/code-challenges/Linked_list/linked-list.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // .insertVal(): insert a value to the head of the linked list
+  insertVal(value) {
+    const node = new Node(value);
+
+    
+  }
+}
